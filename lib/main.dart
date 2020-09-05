c5658fbdb23458f730640288b3b32ceab77ab108
krrahul23 <raulk023@gmail.com>
2020-09-05 17:08:51 +0530

    Added functionalities to the quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +69,33 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                if (answers[questionNumber]) {
+                  setState(
+                    () {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  setState(
+                    () {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.cancel,
+                          color: Colors.red,
+                        ),
+                      );
+                    },
+                  );
+                }
+                questionNumber = questionNumber + 1;
+                if (questionNumber == questions.length) {
+                  questionNumber = 0;
+                }
               },
             ),
           ),
@@ -79,12 +113,40 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                if (!answers[questionNumber]) {
+                  setState(
+                    () {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  setState(
+                    () {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.cancel,
+                          color: Colors.red,
+                        ),
+                      );
+                    },
+                  );
+                }
+                questionNumber = questionNumber + 1;
+                if (questionNumber == questions.length) {
+                  questionNumber = 0;
+                }
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
